d427f4af8651083220689fd2522ca42e44960c03
arm32x <arm32.x@gmail.com>
2021-06-20 20:45:37 -0400

    Initial commit.

---
+++ src/main/java/arm32x/minecraft/armorswap/ArmorSwap.java
@@ -0,0 +1,13 @@
+package arm32x.minecraft.armorswap;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public final class ArmorSwap implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+
+	}
+}
